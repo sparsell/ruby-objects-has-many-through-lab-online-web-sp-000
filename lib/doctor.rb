5f1b8305948c61ae9495f2f57ea88ec5ebea7991
Sonja Parsell <hello@sparsell.com>
2020-05-13 10:16:12 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,7 +14,7 @@ class Doctor
   end
 
   def appointments
-    Appointment.all.select {|appointment| appointment.doctor == self}
+    Appointment.all.collect {|appointment| appointment.doctor == self}
     binding.pry
   end
 
