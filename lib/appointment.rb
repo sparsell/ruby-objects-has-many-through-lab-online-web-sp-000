a867b8ad6faf1cdff9bf0ba237a8d0fbd1072d70
Sonja Parsell <hello@sparsell.com>
2020-05-13 10:02:39 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -2,15 +2,19 @@ class Appointment
 
   attr_accessor :patient, :doctor
 
-@@all = []
+  @@all = []
 
-def initialize
-  @patient = patient
-  @doctor = doctor
-end
+  def initialize
+    @patient = patient
+    @doctor = doctor
+  end
 
-def self.all
-  @@all
-end
+  def self.all
+    @@all
+  end
+
+  def appointments
+    Appointments.all.map {|appointment| appointment.doctor == self}
+  end
 
 end
