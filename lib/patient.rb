3a7eb5918d0bee4189a654287e0676e382c8da70
Sonja Parsell <hello@sparsell.com>
2020-05-13 10:04:48 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,4 +1,17 @@
 class Patient
 
+  attr_accessor :name, :doctor, :appointment
+
+  @@all = []
+
+  def initialize
+    @name = name
+    @doctor = doctor
+    @appointment = appointment
+  end
+
+  def self.all
+    @@all
+  end
 
 end
