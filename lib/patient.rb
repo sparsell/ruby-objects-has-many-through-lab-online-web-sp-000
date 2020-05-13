1634e5450ac3efece840dea8814f24c9a99e8be3
Sonja Parsell <hello@sparsell.com>
2020-05-13 10:41:58 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -21,7 +21,7 @@ class Patient
 
 #returns all appointments associated with this Patient
   def appointments
-    Appointment.all.select {|appointment| appointment.self}
+    Appointment.all.select {|appointment| appointment.patient}
   end
 
 #has many doctors through appointments
