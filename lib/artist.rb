d1a7bc47ea8a917a384d12fe49cac1d69d2ce60a
Sonja Parsell <hello@sparsell.com>
2020-05-13 09:31:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,9 +17,8 @@ class Artist
     Song.all.select {|song| song.artist == self}
   end
 
-  def new_song(name, self, genre)
+  def new_song(name, genre)
     Song.new(name, self, genre)
-
   end
 
   def genres #has many genres, through songs
