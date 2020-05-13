55e8b6dc6fb7566c529f783327b146a7d662d27b
Sonja Parsell <hello@sparsell.com>
2020-05-13 09:27:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,9 @@
 class Song
-  attr_accessor :name, :artist
+  attr_accessor :name, :artist, :genre
 
   @@all = []
 
-  def initialize
+  def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @@all << self
